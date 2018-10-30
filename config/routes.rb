50513591e6cc7bc43dfd77c7897d949354ade22c
Krisztina Kovari <krystonen@gmail.com>
2018-10-30 16:09:46 +0100

    Solution to Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # display all:
+  get 'tasks', to: "tasks#index", as: :tasks
+
+  #create a new task (first get data, then send it)
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # show one item:
+  get 'tasks/:id', to: "tasks#show", as: :task
+
+  # update one item:
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch  "tasks/:id", to: "tasks#update"
+
+  #delete one item:
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# task = Task.new(title: "Task1", details: "Exercises test")
